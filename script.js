cf78f89d4e8fcd50fe9a5ee11be6c36049acd23a
Ari205553 <Ari205553@student-schoolsofwestfield.org>
2025-01-14 17:12:48 +0000

    done

--- script.js
+++ script.js
@@ -2,12 +2,37 @@ var rooms = [elevator, firstFloor, secondFloor, thirdFloor, fourthFloor, fifthFl
 function elevator(floor){
   floor =  prompt(" pick a floor ");
     rooms[floor]();
+    
 }  
-function firstfloor(){
+
+function firstFloor(){
+   alert("First Floor");
     //show background
     //run function
+}
+
+function secondFloor(){
+  alert("Second Floor");
+}
+function thirdFloor(){
+  alert("Third Floor");
+}
+function fourthFloor(){
+  alert("Fourth Floor");
 
 }
+function fifthFloor(){
+  alert(" Fifth Floor"); 
+}
+function sixthFloor(){
+  alert("Sixth Floor");
+}
+function seventhFloor(){
+  alert(" Seventh Floor");
+}
+function eighthFloor(){
+  alert(" Eighth Floor");
+}
 function groundFloor(){
-    alert(" goodbye");
+    alert("Goodbye");
 }
