31746bcbcbb53086158900e2e04923f313a191d9
Ari205553 <Ari205553@student-schoolsofwestfield.org>
2025-01-17 18:45:09 +0000

    fixed floor 2

--- script.js
+++ script.js
@@ -1,43 +1,63 @@
 var rooms = [elevator, firstFloor, secondFloor, thirdFloor, fourthFloor, fifthFloor, sixthFloor];  
+
 function elevator(floor){
   floor =  prompt(" pick a floor ");
     rooms[floor]();
     
 }  
 
-function firstFloor(){
-  let thisFloor = document.getElementById("floorBox");
-  thisFloor.src = "janitorsCloset.png";
-  thisFloor.addEventListener("click", firstMap);
-  firstMap();
-}
-function firstMap(){
-   this.setAttribute("usemap", "first-map");
-   let current= document.getElementById("firstFloor");
-    //show background
-    //run function
+/* General function for popups at each floor. */
+function loadPopUp(image, game){
+  let popup = document.getElementById("popup");
+  // popup.removeChild("pop-image");
+  popup.style.display="block";
+  let popimg = document.createElement("img");
+  popimg.addEventListener("click",game);
+  popimg.id ="pop-image";
+  popimg.src=image;
+  popup.appendChild(popimg);
 }
 
+/* Put all per-floor functions inside the parent */
+function firstFloor(){
+  loadPopUp("janitorsCloset.png", firstMap);
+  // firstMap();
+
+  function firstMap(){
+    this.setAttribute("usemap", "first-map");
+    let current= document.getElementById("firstFloor");
+     //show background
+     //run function
+ }
+
+} //close parent
+
+
 function secondFloor(){
-  let popup = document.getElementById("popup");
-  let popimg = document.createElement("img");
-  popup.addEventListener("click",loadPuzzle);
-  popimg.src="popup.png";
-  popup.appendChild(popimg);
   let thisFloor = document.getElementById("floorBox");
   thisFloor.src = "science.gif";
-  thisFloor.addEventListener("click", secondMap);
-  secondMap();
+  thisFloor.addEventListener("click", showGame);
+  // secondMap();
+
+  function showGame(){
+    loadPopUp("desk.png",loadPuzzle);
+  }
   function loadPuzzle(){
+    let popimg = document.getElementById("pop-image");
+    popup.removeChild(popimg);
     const iframe = document.createElement("iframe");
-    iframe.src = "puzzle-index.html";
+    iframe.src = "puzzle/index.html";
     popup.appendChild(iframe);
   }
-}
-function secondMap(){
-this.setattribute("use","second-Map")
-let current= document.getElementById("secondFloor");
-}
+
+  function secondMap(){
+    this.setattribute("use","second-Map")
+    let current= document.getElementById("secondFloor");
+  }
+
+} // close parent
+
+
 function thirdFloor(){
   alert("Third Floor");
 }
